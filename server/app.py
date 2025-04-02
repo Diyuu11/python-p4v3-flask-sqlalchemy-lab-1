2e1563dd5a6d0c89d6dda8a9a4ca734dede4608a
hamdiabdiaden <diagodu198@gmail.com>
2025-04-02 08:00:54 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -20,7 +20,21 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return make_response(earthquake.to_dict(), 200)
+    else:
+        return make_response({"message": f"Earthquake {id} not found."}, 404)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    return make_response({
+        "count": len(earthquakes),
+        "quakes": [quake.to_dict() for quake in earthquakes]
+    }, 200)
 
 
 if __name__ == '__main__':
